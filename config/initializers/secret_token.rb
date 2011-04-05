858cba1fecdf1eaac027f1b8393404e6ea7f9d66
Blake Urmos <blakeurmos@gmail.com>
2011-04-05 19:50:22 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bocasta::Application.config.secret_token = '61ffadbfc5a763de95db649809fa944ba98112a24459fd1e52788a936935f2dead2a950d9a61ad90fa95cda2f94c88e829bf151857b423e3810fa9be2f8a011d'
